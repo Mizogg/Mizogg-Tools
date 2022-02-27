d790a05b7f3aef63481001f9a74039e50c066030
Mizogg <88630056+Mizogg@users.noreply.github.com>
2022-02-27 20:33:02 +0000

    Update Miz_Tools_bit_colour.py

--- Miz_tools_bit/Miz_Tools_bit_colour.py
+++ Miz_tools_bit/Miz_Tools_bit_colour.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Made by Mizogg Tools to Help Look for Bitcoin. Good Luck and Happy Hunting Miz_Tools_bit_colour.py Version 2 Donations 3GCypcW8LWzNfJEsTvcFwUny3ygPzpTfL4
+Made by Mizogg Tools to Help Look for Bitcoin. Good Luck and Happy Hunting Miz_Tools_bit_colour.py Version 3 Donations 3GCypcW8LWzNfJEsTvcFwUny3ygPzpTfL4
  23 Bitcoin Tools
 Using Bit Library made in Python
 https://mizogg.co.uk
@@ -312,12 +312,16 @@ while True:
         print(colour_yellow + 'Hexadecimal to Decimal Tool' + colour_reset)
         HEX = str(input('Enter Your Hexadecimal HEX Here : '))
         dec = int(HEX, 16)
-        print('\nHexadecimal = ',HEX, '\nTo Decimal = ', dec)
+        length = len(bin(dec))
+        length -=2
+        print('\nHexadecimal = ',HEX, '\nTo Decimal = ', dec, '  bits ', length)
     elif start == 6:
         print(colour_yellow + 'Decimal to Hexadecimal Tool' + colour_reset)
         dec = int(input('Enter Your Decimal DEC Here : '))
         HEX = "%064x" % dec
-        print('\nDecimal = ', dec, '\nTo Hexadecimal = ', HEX)
+        length = len(bin(dec))
+        length -=2
+        print('\nDecimal = ', dec, '  bits ', length, '\nTo Hexadecimal = ', HEX)
     elif start == 7:
         prompthex=colour_yellow +  '''
     ************************* Hexadecimal to Bitcoin Address Tool ************************* 
@@ -421,7 +425,27 @@ while True:
     elif start == 9:
         print('Mnemonic 12/15/18/21/24 Words to Bitcoin Address Tool')
         wordlist = str(input('Enter Your Mnemonic Words = '))
-        mnemo = Mnemonic("english")
+        Lang = int(input(' Choose language 1.english, 2.french, 3.italian, 4.spanish, 5.chinese_simplified, 6.chinese_traditional, 7.japanese or 8.korean '))
+        if Lang == 1:
+            Lang1 = "english"
+        elif Lang == 2:
+            Lang1 = "french"
+        elif Lang == 3:
+            Lang1 = "italian"
+        elif Lang == 4:
+            Lang1 = "spanish"
+        elif Lang == 5:
+            Lang1 = "chinese_simplified"
+        elif Lang == 6:
+            Lang1 = "chinese_traditional"
+        elif Lang == 7:
+            Lang1 = "japanese"
+        elif Lang == 8:
+            Lang1 = "korean"
+        else:
+            print("WRONG NUMBER!!! Starting with english")
+            Lang1 = "english"
+        mnemo = Mnemonic(Lang1)
         mnemonic_words = wordlist
         seed = mnemo.to_seed(mnemonic_words, passphrase="")
         data_wallet()
@@ -720,12 +744,32 @@ while True:
         else:
             print("WRONG NUMBER!!! Starting with 24 Words")
             s1 = 256
+        Lang = int(input(' Choose language 1.english, 2.french, 3.italian, 4.spanish, 5.chinese_simplified, 6.chinese_traditional, 7.japanese or 8.korean '))
+        if Lang == 1:
+            Lang1 = "english"
+        elif Lang == 2:
+            Lang1 = "french"
+        elif Lang == 3:
+            Lang1 = "italian"
+        elif Lang == 4:
+            Lang1 = "spanish"
+        elif Lang == 5:
+            Lang1 = "chinese_simplified"
+        elif Lang == 6:
+            Lang1 = "chinese_traditional"
+        elif Lang == 7:
+            Lang1 = "japanese"
+        elif Lang == 8:
+            Lang1 = "korean"
+        else:
+            print("WRONG NUMBER!!! Starting with english")
+            Lang1 = "english"
         display = int(input('1=Full Display (Slower) 2=Slient Mode (Faster) : '))
         while True:
             data=[]
             count += 1
             total += 20
-            mnemo = Mnemonic("english")
+            mnemo = Mnemonic(Lang1)
             mnemonic_words = mnemo.generate(strength=s1)
             seed = mnemo.to_seed(mnemonic_words, passphrase="")
             entropy = mnemo.to_entropy(mnemonic_words)
@@ -1170,4 +1214,3 @@ while True:
       
     else:
         print(colour_red + "WRONG NUMBER!!! MUST CHOSE 1 - 23 " + colour_reset)
-        break
