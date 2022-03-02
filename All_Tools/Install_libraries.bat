5db772502feaa8d339299c12149dfac75bbbcff7
Mizogg <88630056+Mizogg@users.noreply.github.com>
2022-03-02 23:26:53 +0000

    Delete All_Tools directory

--- All_Tools/Install_libraries.bat
+++
@@ -1,6 +0,0 @@
-pip install bit
-pip install requests
-pip install mnemonic
-pip install bip32utils
-pip install base58
-pip install hdwallet
--- All_Tools/Start_Miz_Tools_all.bat
+++
@@ -1,6 +0,0 @@
-@Echo off
-title Miz_Tools_all.py
-Pushd "%~dp0"
-:loop
-python Miz_Tools_all.py
-goto loop
