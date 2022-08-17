c7baf5646e70854e2d20010a11159791ce04b611
Mizogg <88630056+Mizogg@users.noreply.github.com>
2022-08-17 22:59:27 +0100

    Update Install_libraries.bat

--- Telebot/Install_libraries.bat
+++ Telebot/Install_libraries.bat
@@ -2,7 +2,7 @@ pip install bit
 pip install requests
 pip install rich
 pip install base58
-pip install simplebloomfliter
+pip install simplebloomfilter
 pip install bitarray==1.9.2
 pip install pyTelegramBotAPI
 pip install lxml
