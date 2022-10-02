3d4d489c70374a8ff3955194e3fc81cd1d89bd13
Mizogg <88630056+Mizogg@users.noreply.github.com>
2022-10-02 15:54:27 +0100

    Update Install_libraries.bat
    pip install httplib2

--- Telebot/Install_libraries.bat
+++ Telebot/Install_libraries.bat
@@ -1,8 +1,7 @@
 pip install bit
-pip install requests
 pip install rich
 pip install base58
 pip install simplebloomfilter
 pip install bitarray==1.9.2
 pip install pyTelegramBotAPI
-pip install lxml
+pip install httplib2
